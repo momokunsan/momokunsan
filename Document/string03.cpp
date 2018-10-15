0f11151cb8953883b99a6fc18cc5aabcb2f7dceb
momokunsan <momo19990101@gmail.com>
2018-10-15 23:47:54 +0900

    フォルダの作成

---
+++ Document/string03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	/*“üo—Í*/
+	string a = "hoge";
+	cout << a << endl;
+
+	/*‘ã“ü*/
+
+	string b;
+	b = 'a';
+
+	string c;
+	c = a;
+
+	string z;
+	cin >> z;
+	return 0;
+}
